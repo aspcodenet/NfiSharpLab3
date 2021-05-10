0c542081da93f7feaf8f740cded2316a38575c90
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-10 16:38:25 +0200

    Add project files.

---
+++ NfiSharpLab3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NfiSharpLab3
+{
+    class Program
+    {
+        /*
+         Skapa en variabel string name med ditt namn
+        Skapa en int age med din ålder.
+        Skriv sedan ut Jag heter Kalle (innehållet i name) och är 27(innehållet i age) år.
+
+
+         */
+        static void Main(string[] args)
+        {
+
+            var name = "Stefan";
+            var age = 48;
+            Console.WriteLine(name);
+            if(age > 48)
+                Console.WriteLine("Herlölo");
+            Console.WriteLine(age);
+
+        }
+    }
+}
